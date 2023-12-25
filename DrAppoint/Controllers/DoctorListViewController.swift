d333deabeed7fce5d9567dc927f7cb95b35b420d
matrecode <akshaymatrecode@gmail.com>
2023-12-25 16:48:23 +0530

    working on table

--- DrAppoint/Controllers/DoctorListViewController.swift
+++ DrAppoint/Controllers/DoctorListViewController.swift
@@ -8,23 +8,40 @@
 import UIKit
 import CoreData
 
-class DoctorListViewController: UIViewController{
+class DoctorListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
    
+    
+    private let tableView = UITableView()
+    private var doctors: [Doctor] = []
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         setupUI()
-        let fetchRequest: NSFetchRequest<Doctor> = Doctor.fetchRequest()
+        tableView.frame = view.bounds
+        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        tableView.dataSource = self
+        tableView.delegate = self
+        
 
+        view.addSubview(tableView)
+        
+        let fetchRequest: NSFetchRequest<Doctor> = Doctor.fetchRequest()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "DoctorCell")
+//        do {
+//            let doctors = try CoreDataManager.shared.persistentContainer.viewContext.fetch(fetchRequest)
+//            for doctor in doctors {
+//                print("Name: \(doctor.name ?? "N/A"), Speciality: \(doctor.speciality ?? "N/A"), Phone: \(doctor.phone ?? "N/A")")
+//            }
+//        } catch {
+//            print("Error fetching doctors: \(error.localizedDescription)")
+//        }
         do {
-            let doctors = try CoreDataManager.shared.persistentContainer.viewContext.fetch(fetchRequest)
-            for doctor in doctors {
-                print("Name: \(doctor.name ?? "N/A"), Speciality: \(doctor.speciality ?? "N/A"), Phone: \(doctor.phone ?? "N/A")")
-            }
-        } catch {
-            print("Error fetching doctors: \(error.localizedDescription)")
-        }
+               doctors = try CoreDataManager.shared.persistentContainer.viewContext.fetch(fetchRequest)
+               tableView.reloadData()
+           } catch {
+               print("Error fetching doctors: \(error.localizedDescription)")
+           }
     }
     
 
@@ -35,6 +52,21 @@ class DoctorListViewController: UIViewController{
 
         navigationItem.rightBarButtonItems = [plusButton, editButton]
      }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return doctors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DoctorCell", for: indexPath)
+        let doctor = doctors[indexPath.row]
+
+        // Configure the cell with doctor data
+        cell.textLabel?.text = doctor.name ?? "N/A"
+        cell.detailTextLabel?.text = "Speciality: \(doctor.speciality ?? "N/A"), Phone: \(doctor.phone ?? "N/A")"
+
+        return cell
+    }
     
 
     
